bc186089a7ad4e8b8a688e30cac878c893462483
aizavilladolid <aizavilladolid8@gmail.com>
2026-04-30 22:11:23 +0800

    I changed a character name

--- Main.java
+++ Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<GameCharacter> party = new ArrayList<>();
 
-        party.add(new Warrior("Leon", 100, 20));
-        party.add(new Mage("Selene", 80, 25));
-        party.add(new Archer("Robin", 90, 18));
+        party.add(new Warrior("Aiza", 100, 20));
+        party.add(new Mage("Ian", 80, 25));
+        party.add(new Archer("Ashley", 90, 18));
 
         for (GameCharacter character : party) {
             character.describeClass();
